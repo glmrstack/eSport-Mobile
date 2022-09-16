caef8c59c47da4e629b980175f09cff3430d5fcc
Gilmar Junior <gilmar.stack@gmail.com>
2022-09-16 11:40:06 -0300

    Adjust sample

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,16 +3,16 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: 'center'
   },
   logo: {
     width: 214,
     height: 120,
     marginTop: 96,
-    marginBottom: 32,
+    marginBottom: 32
   },
   contentList: {
     paddingLeft: 32,
-    paddingRight: 40,
+    paddingRight: 40
   }
 });
